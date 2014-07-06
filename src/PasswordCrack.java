738ed85bcbed57a690752566b2dd28be4a479767
johncade <jcgriffin@utexas.edu>
2014-07-06 17:59:59 -0500

    Initial classes

--- src/PasswordCrack.java
+++ src/PasswordCrack.java
@@ -8,4 +8,14 @@ public class PasswordCrack {
 
     private static ArrayList<String> dictionary;
     private static ArrayList<User> users;
+
+    public static void main(String[] args){
+
+    }
+    public static void scanArgs(String[] args){
+
+    }
+    public static void run(){
+
+    }
 }
