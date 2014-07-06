1e146931944c0321084fccd9b6eb21a71be56bb7
johncade <jcgriffin@utexas.edu>
2014-07-06 17:58:38 -0500

    Initial classes

---
+++ src/PasswordCrack.java
@@ -0,0 +1,11 @@
+/**
+ * Created by madgriff on 7/6/14.
+ */
+import java.util.*;
+import java.io.*;
+
+public class PasswordCrack {
+
+    private static ArrayList<String> dictionary;
+    private static ArrayList<User> users;
+}
